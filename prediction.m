711f684d3a334d880782110040bc3c1e22b13090
AKHILDAS KS <61543026+akhilaku@users.noreply.github.com>
2020-04-28 18:53:14 +0530

    Update prediction.m

--- prediction.m
+++ prediction.m
@@ -4,5 +4,6 @@ deepnet = alexnet;
 #to product the subject in image stored in variable 'img1'.
 #store the result in variable 'pred1'.
 img1 = imread('file01.jpg');
+#to show the image
 imshow(img1);
 pred1 = classify(deepnet,img1);
